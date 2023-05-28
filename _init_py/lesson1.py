c9e13701cb3ad7861035011addfa3378b378cf8b
Геннадий Матвиенко <gennady39@hotmail.com>
2023-05-28 17:13:13 +0300

    добавлены задачи №6,8

--- _init_py/lesson1.py
+++ _init_py/lesson1.py
@@ -12,3 +12,21 @@ else:
 k = input("Введите общее количество журавликов ");
 ps = int(str(k))//6;
 print("Петя и Сережа сделали по", ps, ", а Катя сделала", ps*4);
+
+# Задача 6
+
+s = str(input("Введите номер билета "))
+sum1=int(s[0])+int(s[1])+int(s[2])
+sum2=int(s[3])+int(s[4])+int(s[5])
+if sum1==sum2:
+  print('Счастливый')
+else:
+  print('Обычный')
+
+
+# Задача 8
+
+n, m, k = int(input('Введите  1-ю сторону: ')), int(input('Введите 2-ю сторону: ')), int(input('Введите кол-во долек: '))
+if k%n == 0 or k%m == 0:
+    print('Yes')
+else: print('No')
